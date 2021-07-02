ab94046634b125ddc8534b5801aebbc0fbea439f
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-02 20:33:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+(async() => {
+  const browser = await puppeteer.launch({
+     //args: [ '--proxy-server=http://ip:port' ]
+  });
+  const page = await browser.newPage();
+  await page.goto('https://api.myip.com');
+  const html = await page.evaluate(() => {
+      return document.body.outerHTML
+  })
+  console.log(html)
+  await browser.close();
+})();
