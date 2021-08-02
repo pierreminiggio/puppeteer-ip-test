981c073b2b65cf6e3065c5f3f6bc671e312041be
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-08-02 19:41:04 +0200

    process proxy

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
-const proxyChain = require('proxy-chain');
 
 (async() => {
 
@@ -9,12 +8,43 @@ const proxyChain = require('proxy-chain');
     '--no-sandbox'
   ]
 
+  let proxy = null
+  let needsAuth = false
+  let username = null
+  let password = null
+
   if (process.argv.length === 3) {
-    args.push('--proxy-server=' + await proxyChain.anonymizeProxy(process.argv[2]))
+    const inputProxy = process.argv[2]
+    const splitProxy = inputProxy.split('@')
+    needsAuth = splitProxy.length > 1
+
+    if (needsAuth) {
+      const usernameAndPasswordAndMethod = splitProxy[0]
+      const methodSeparator = '://'
+      const splitUsernameAndPasswordAndMethod = usernameAndPasswordAndMethod.split(methodSeparator)
+      const method = splitUsernameAndPasswordAndMethod[0]
+      const usernameAndPassword = splitUsernameAndPasswordAndMethod[1]
+      const splitUsernameAndPassword = usernameAndPassword.split(':')
+      username = splitUsernameAndPassword[0]
+      password = splitUsernameAndPassword[1]
+      proxy = method + methodSeparator + splitProxy[1]
+    } else {
+      proxy = inputProxy
+    }
   }
+
+  if (proxy) {
+    args.push('--proxy-server=' + proxy)
+  }
+  
 
   const browser = await puppeteer.launch({args});
   const page = await browser.newPage();
+
+  if (needsAuth) {
+    await page.authenticate({username, password})
+  }
+
   try {
     await page.goto('https://api.myip.com');
   } catch(error) {
